f81afb6ccedffadda514754e990ac6a68b7adcd5
Eric Lobdell <eric@ericlobdell.me>
2017-07-27 21:16:11 -0400

    added require guards

--- EnGuarde/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EnGuarde
-{
-    public class Class1
-    {
-    }
-}
---
+++ EnGuarde/Guards.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EnGuarde
+{
+  public static class Guards
+  {
+    public static void Require(object o, string msg)
+    {
+      if (o is string && string.IsNullOrWhiteSpace(o as string))
+        throw new ArgumentException(msg);
+
+      if (o == null)
+        throw new ArgumentException(msg);
+    }
+
+    public static void Require(params (object value, string errorMessage)[] args)
+    {
+      foreach (var a in args)
+        Require(a.value, a.errorMessage);
+    }
+  }
+}
