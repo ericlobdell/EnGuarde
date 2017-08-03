68511a6644f0138bfd15f1671a5f18c3dc2dd4ef
Eric Lobdell <ericlobdell@users.noreply.github.com>
2017-08-03 12:57:25 -0400

    updated Guards with fluent syntax

--- EnGuarde/Guards.cs
+++ EnGuarde/Guards.cs
@@ -4,19 +4,46 @@ namespace EnGuarde
 {
   public static class Guards
   {
-    public static void Require(object o, string msg)
+    private static GuardInvoker invoker = new GuardInvoker();
+
+    public static GuardInvoker Require(object o, string msg) =>
+      invoker.Require(o, msg);
+
+    public static GuardInvoker Require(params (object value, string errorMessage)[] args) =>
+      invoker.Require(args);
+
+    public static GuardInvoker ThrowIf(bool condition, string msg) =>
+      invoker.ThrowIf(condition, msg);
+    
+  }
+
+  public class GuardInvoker
+  {
+    public GuardInvoker Require(object o, string msg)
     {
       if (o is string && string.IsNullOrWhiteSpace(o as string))
         throw new ArgumentException(msg);
 
       if (o == null)
         throw new ArgumentException(msg);
+
+      return this;
     }
 
-    public static void Require(params (object value, string errorMessage)[] args)
+    public GuardInvoker Require(params (object value, string errorMessage)[] args)
     {
       foreach (var a in args)
         Require(a.value, a.errorMessage);
+
+      return this;
+    }
+
+    public GuardInvoker ThrowIf(bool condition, string msg)
+    {
+      if (condition)
+        throw new ArgumentException(msg);
+
+      return this;
     }
   }
 }
